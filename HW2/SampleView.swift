80bdd383436a373d420ffac7b2d844706dc4c669
fatality0 <48532377+fatality0@users.noreply.github.com>
2021-12-25 12:49:25 +0300

    initial commit

---
+++ HW2/SampleView.swift
@@ -0,0 +1,37 @@
+//
+//  SampleView.swift
+//  HW2
+//
+//  Created by Alper Demir on 25.12.2021.
+//  Copyright © 2021 Alper Demir. All rights reserved.
+//
+
+import UIKit
+
+final class SampleView: UIView {
+
+    @IBOutlet private var contentView: UIView!
+    @IBOutlet private weak var textField: UITextField!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+
+    private func initialize() {
+        Bundle.main.loadNibNamed("SampleView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+
+    @IBAction func onSubmitButtonTapped(_ sender: UIButton) {
+        let text = textField.text
+    }
+
+}
