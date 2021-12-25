0fdd14971e1c4d9076f62abfea95c97ee970a5fe
fatality0 <48532377+fatality0@users.noreply.github.com>
2021-12-25 11:37:29 +0300

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by Alper Demir on 25.12.2021.
+//  Copyright © 2021 Alper Demir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
